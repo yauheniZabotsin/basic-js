1d4090f1c97dde52d355de8a7a5e6177dcf6f914
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2022-10-14 23:05:06 +0300

    task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  if (n.toString().length === 1) {
+		return n;
+	} else {
+		let sum = String(n).split('').map(Number).reduce((a, b) => a + b);
+		return getSumOfDigits(sum)
+	}
 }
 
 module.exports = {
